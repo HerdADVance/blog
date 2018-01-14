edf372339b5ef82e209047e10f6c3ea14f7b20e9
m <m@ms-MacBook-Pro.local>
2018-01-14 01:23:49 -0500

    First push

---
+++ article.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alex Vance</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/normalize.css"/>
+	<link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700" rel="stylesheet">
+	<meta property="og:title" content="Blog Title" />
+	<meta property="og:image" content="Image Link"/>
+	<meta property="og:site_name" content="Blog Title" />
+	<meta property="og:url" content="URL link" />
+	<meta property="og:description" content="Description" />
+</head>
+<body>
+	<header class="site-header wrap left">
+		<span><a class="site-logo" href="/">Alex Vance</a></span>
+		<nav class="nav">
+			<ul>
+				<li><a href="">About</a></li>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Portfolio</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div class="article-image"></div>
+	<div class="parallax">
+		<main class="article-main">
+			<article class="article wrap">
+				<header class="article-header">
+					<img class="article-author-image" src="/assets/images/alex-vance.jpg" alt="Alex Vance">
+					<h1 class="article-title center">Every Single West Virginian Has Taken This Same Goddamn Picture</h1>
+					<span class="article-byline">
+						<strong>Alex Vance</strong><br/>
+						<time datetime="2017-12-26">December 26, 2017</time>
+					</span>
+				</header>
+				
+				<main>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam risus velit, tempus ac vehicula at, eleifend eget neque. Aenean vel vestibulum quam. Ut congue fringilla mattis. Morbi convallis sollicitudin nunc, vitae lacinia neque consequat a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi sed odio sit amet nisi lobortis dictum sed a massa. Maecenas venenatis enim non erat iaculis auctor.</p>
+					<h2>Best Marshall players</h2>
+					<ul>
+						<li>Randy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy MossRandy Moss</li>
+						<li>Chad Pennington</li>
+						<li>Byron Leftwich</li>
+						<li>Rakeem Cato</li>
+						<li>Troy Brown</li>
+					</ul>
+					<p>Morbi ac consequat turpis. In nec tortor ut odio viverra finibus. Suspendisse sagittis sed quam sit amet euismod. Vivamus viverra enim in dui eleifend, vel feugiat diam interdum. Vestibulum nec purus vel ligula tempus congue ac sit amet tortor. Ut consectetur viverra maximus. Proin dignissim euismod lacus ut tempus. Mauris vestibulum urna quis congue pretium. Phasellus molestie nisl sapien, eu elementum nibh consequat nec. Donec lorem lectus, eleifend eu ipsum ac, dapibus vulputate tellus. Vivamus dignissim mollis orci, nec efficitur ante cursus quis. Maecenas et metus elit. Pellentesque laoreet cursus dui, commodo varius odio ullamcorper non. Aenean volutpat ex at purus gravida commodo.</p>
+					<h2>Subheader Here</h2>
+					<p>Fusce pretium ut elit eget bibendum. Integer quis lobortis magna, accumsan finibus magna. Donec erat est, congue quis felis et, vulputate blandit ipsum. Proin sit amet velit pretium, ullamcorper nulla quis, lacinia eros. Nam non mi id velit consectetur eleifend et eu lacus. Suspendisse dictum ultricies facilisis. Aenean quis pretium mauris. Vestibulum malesuada, lacus eu imperdiet commodo, ipsum diam interdum nunc, consectetur cursus nulla tellus et nulla.</p>
+					<p><img src="/assets/images/vikes-panthers.jpg"></p>
+					<p>Etiam posuere, dui ac aliquet dapibus, purus est vestibulum sem, eu volutpat lacus felis vitae est. Nunc scelerisque tincidunt odio, id blandit orci tempor et. Sed vel dignissim odio. Etiam mollis arcu id sem rhoncus aliquam id a lectus. Integer ornare felis sollicitudin, viverra sem quis, egestas felis. Ut hendrerit orci id semper porttitor. Sed iaculis eget magna vel varius. Donec vitae sem quis ipsum vehicula posuere eget ac tellus. Nam scelerisque, felis in maximus hendrerit, ipsum mauris suscipit odio, ut ultrices nisi turpis eu ligula. Donec velit mauris, feugiat quis posuere at, laoreet vel velit. Cras odio est, elementum id ultrices sed, ultrices vitae nisi.</p>
+					<ul>
+						<li>Etiam posuere, dui ac aliquet dapibus, purus est vestibulum sem, eu volutpat lacus felis vitae est. Nunc scelerisque tincidunt.</li>
+						<li>Another part of the list</li>
+						<li>Yep, another item</li>
+						<li>Purus est vestibulum sem, eu volutpat lacus felis vitae est. Nunc scelerisque tincidunt odio, id blandit orci tempor et. Sed vel dignissim odio. Etiam mollis arcu.</li>
+						<li>Penis.</li>
+					</ul>
+					<p>Praesent pharetra at libero sit amet auctor. Suspendisse ut maximus ante, a hendrerit urna. In risus felis, pellentesque vitae felis nec, placerat sollicitudin eros. Donec purus tellus, hendrerit id bibendum ac, condimentum sit amet elit. Suspendisse molestie ante eget blandit tempor. Suspendisse potenti. Donec sollicitudin tellus pharetra, tristique magna commodo, euismod tellus. Maecenas nibh orci, tempor sit amet eleifend in, vulputate vel lorem. Suspendisse in dolor at odio vulputate aliquet. Suspendisse auctor urna nulla, vitae auctor felis iaculis nec. Praesent id convallis velit. Sed non augue ullamcorper, eleifend nulla sed, ornare erat.</p>
+				</main>
+
+				<footer class="article-footer">
+					<p>Article Footer</p>
+				</footer>
+
+			</article>
+
+			<section class="wrap">
+				<h1 class="section-title">Related Articles</h1>
+				<div class="related-articles">
+					<div class="related-article">
+						<a href="/">
+							<div class="related-article-image" style="background-image:url(/assets/images/siesta-key-sunset.jpg);"></div>
+							<h2 class="related-article-title">Not Good For Bubbers</h2>
+						</a>
+					</div>
+					<div class="related-article">
+						<a href="/">
+							<div class="related-article-image" style="background-image:url(/assets/images/vesuvius-2.jpg);"></div>
+							<h2 class="related-article-title">Surprisingly, No Ohioans Have Driven into this Lake</h2>
+						</a>
+					</div>
+					<div class="related-article">
+						<a href="/">
+							<div class="related-article-image" style="background-image:url(/assets/images/winston-salem-snow.jpg);"></div>
+							<h2 class="related-article-title">What the hell? Why is it snowing in North Carolina in early December?</h2>
+						</a>
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<footer class="site-footer wrap">
+			<p>&copy; Alex Vance 2018</p>
+			<p>But seriously, why is this even here? Who's going to steal this?</p>
+		</footer>
+
+	</div> <!--Parallax-->
+
+	<!--button class="to-top">^</button-->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alex Vance</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/normalize.css"/>
+	<link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700" rel="stylesheet">
+	<meta property="og:title" content="Blog Title" />
+	<meta property="og:image" content="Image Link"/>
+	<meta property="og:site_name" content="Blog Title" />
+	<meta property="og:url" content="URL link" />
+	<meta property="og:description" content="Description" />
+</head>
+<body>
+	<header class="site-header wrap left">
+		<span><a class="site-logo" href="/">Alex Vance</a></span>
+		<nav class="nav">
+			<ul>
+				<li><a href="">About</a></li>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Portfolio</a></li>
+				<li><a href="">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<section class="home-featured" style="background-image: radial-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.9)), url('/assets/images/new-river-overlook-feet.jpg');">
+			<a href="/article.php">
+				<h1>Everyone in West Virginia Has Taken This Same Exact Goddamn Picture</h1>
+			</a>
+		</section>
+
+		<section class="home-sections wrap">
+			<button></button>
+			<button>Code</button>
+			<button>Sports</button>
+			<button>Politics</button>
+			<button>Travel</button>
+			<button>Fitness</button>
+		</section>
+		
+	</main>
+
+	<footer class="site-footer wrap">
+		<p>&copy; Alex Vance 2018</p>
+		<p>But seriously, why is this even here? Who's going to steal this?</p>
+	</footer>
+
+	<!--button class="to-top">^</button-->
+</body>
+</html>
